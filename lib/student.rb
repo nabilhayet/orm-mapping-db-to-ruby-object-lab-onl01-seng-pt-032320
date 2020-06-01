8f1acb1c5daa6573c7f587b8db3bf077aad36ba8
Nabil Hayet <nabil.bracu@gmail.com>
2020-06-01 00:50:03 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -24,8 +24,9 @@ class Student
       where name = ?
       limit 1
     SQL
-    DB[:conn].execute(sql, name).map do |row|
-    self.new_from_db(row)
+      DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+      end 
   end
 
   def save
