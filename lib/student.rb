bcf66bc8880f78ece75edddd695c6b0ffef626bf
Nabil Hayet <nabil.bracu@gmail.com>
2020-06-01 01:00:38 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -71,4 +71,14 @@ class Student
     DB[:conn].execute(sql).map do |value|
     end
   end
+
+  def self.students_below_12
+    sql = <<-SQL
+     select *
+     from students
+     where grade < 12 
+    SQL
+    DB[:conn].execute(sql).map do |value|
+    end
+  end
 end
